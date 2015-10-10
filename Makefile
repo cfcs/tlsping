37c3801bb916dc5042a8c1628575603f403ee462
cfcs <none>
2015-10-10 13:00:58 +0200

    move to topkg build system

--- Makefile
+++
@@ -1,9 +0,0 @@
-client:
-	ocamlfind ocamlopt -package ctypes,tls,rresult -linkpkg \
-		generate_ping.ml \
-	  -o tls-ping-client
-
-clean:
-	rm *.o *.cmx *.cmo *.cmi \
-		tls-ping-client \
-		2>/dev/null ; true
---
+++ pkg/build.ml
@@ -0,0 +1,8 @@
+#!/usr/bin/env ocaml
+#directory "pkg";;
+#use "topkg.ml";;
+
+let () = Pkg.describe "tlspingd" ~builder:`OCamlbuild [
+    Pkg.lib "pkg/META";
+    Pkg.bin ~auto:true "tls_ping_client";
+    Pkg.doc "idea.md"; ]
