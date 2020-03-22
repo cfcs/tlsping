7d285d075031956e8eb1fd23ccd2bd7e0e0e5a6c
hark <hark@puscii.nl>
2020-03-22 16:11:50 +0000

    socat.sh

--- testscript/socat.sh
+++ testscript/socat.sh
@@ -9,6 +9,6 @@ GD="`git rev-parse --show-toplevel`"
 cd "$GD"/_test/
 
 
-FP=`"$GD"/testscript/getfingerprint.sh $HOST $PORT`
+FP=`"$GD"/testscript/getfingerprint.sh "$HOST" "$PORT"`
 
 socat stdio socks4a:localhost:"$HOST":"$PORT",socksport=6667,socksuser="$FP"
