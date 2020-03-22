74a1f92480c4169641c0f234dbb911d50eaa6a17
whohoho <whoops@eduif.nl>
2020-03-22 12:43:55 +0100

    Update testscript/runserver.sh
    Co-Authored-By: C For C's Sake <cfcs@users.noreply.github.com>

--- testscript/runserver.sh
+++ testscript/runserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -eu
 GD="`git rev-parse --show-toplevel`"
 
 cd $GD/_test/
@@ -7,5 +7,4 @@ cd $GD/_test/
 #Usage: tls_ping_server [OPTION]... CA-PUBLIC-CERT PROXY-PUBLIC-CERT PROXY-SECRET-KEY
 
 $GD/_build/default/tls_ping_server.exe --verbosity debug ca.public.certificate proxy.public.certificate proxy.secret.key
-
 
