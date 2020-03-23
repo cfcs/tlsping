dbb9e14f8921c317af58ae066ae178bbad6e9499
hark <hark@puscii.nl>
2020-03-23 11:29:01 +0000

    fix stdout > stderr

--- testscript/getfingerprint.sh
+++ testscript/getfingerprint.sh
@@ -5,7 +5,7 @@ set -eux
 HOST="$1"
 PORT="$2"
 
-echo "sha256 fingerprint of public key: " 1>2
+echo "sha256 fingerprint of public key: " 1>&2
 openssl s_client -showcerts -servername "$HOST" -connect ${HOST}:${PORT} </dev/null 2>/dev/null | openssl x509 -pubkey -noout | grep -v 'PUBLIC KEY' | base64 -d | sha256sum | cut -d ' ' -f 1
 
 
